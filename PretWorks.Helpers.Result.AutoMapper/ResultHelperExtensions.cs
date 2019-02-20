a29986342f3e915bd248c99130b941dff91b2baa
Rob van der Werf <rob@pretworks.com>
2019-02-20 11:23:38 +0100

    Automapper extension

---
+++ PretWorks.Helpers.Result.AutoMapper/ResultHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PretWorks.Helpers.Result.Interfaces;
+
+namespace PretWorks.Helpers.Result.AutoMapper
+{
+    public static class ResultHelperExtensions
+    {
+        /// <summary>
+        /// Map value to TValue using automapper
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static IResult<TValue> WithMappedValue<TValue>(this IResult<TValue> result, object value)
+        {
+            result.Value = Mapper.Map<TValue>(result);
+
+            return result;
+        }
+    }
+}
